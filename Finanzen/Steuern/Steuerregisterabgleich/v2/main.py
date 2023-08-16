7b294bd49a54c94ab7245772d39b567730e4b52d
tj001 <43321167+timonj0@users.noreply.github.com>
2023-08-16 14:08:47 +0000

    Add load_data()

--- Finanzen/Steuern/Steuerregisterabgleich/v2/main.py
+++ Finanzen/Steuern/Steuerregisterabgleich/v2/main.py
@@ -2,13 +2,64 @@
 # Author: Timon Jakob
 # Date: 08.2023
 
+import os
+import time
+
+import pandas
+
+def load_data():
+    '''Function to load the data'''
+
+    # Get all files for the citizen register
+    print(f"Reading citizen register files: {os.listdir('register/reg_cit')}")
+    files = os.listdir('register/reg_cit')
+
+    # Get a dataframe for every file
+    dfs = []
+    for file in files:
+        dataframe = pandas.read_excel('register/reg_cit/' + file)
+        print(f"Register file {file} read successfully")
+        dfs.append(dataframe)
+
+    # Combine all dataframes
+    dataframe_cit = pandas.concat(dfs, ignore_index=True)
+
+    # Get all files for the tax register
+    print(f"Reading tax register files: {os.listdir('register/reg_tax')}")
+    files = os.listdir('register/reg_tax')
+
+    # Get a dataframe for every file
+    dfs = []
+    for file in files:
+        dataframe = pandas.read_excel('register/reg_tax/' + file)
+        print(f"Register file {file} read successfully")
+        dfs.append(dataframe)
+
+    # Combine all dataframes
+    dataframe_tax = pandas.concat(dfs, ignore_index=True)
+
+    # Get all files for the qst register
+    print(f"Reading qst register files: {os.listdir('register/reg_qst')}")
+    files = os.listdir('register/reg_qst')
+
+    # Get a dataframe for every file
+    dfs = []
+    for file in files:
+        dataframe = pandas.read_excel('register/reg_qst/' + file)
+        print(f"Register file {file} read successfully")
+        dfs.append(dataframe)
+
+    # Combine all dataframes
+    dataframe_qst = pandas.concat(dfs, ignore_index=True)
+
+    return dataframe_cit, dataframe_tax, dataframe_qst
 
 
 if __name__ == "__main__":
     start_time = time.time()
-    dataframe_cit, dataframe_tax = load_data()
+    dataframe_cit, dataframe_tax, dataframe_qst = load_data()
     dataframe_cit = prepare_data_cit(dataframe_cit)
-    dataframe_tax = prepare_data_tax(dataframe_tax)
+    dataframe_tax = prepare_data_tax(dataframe_tax, dataframe_qst)
     dataframe_missing = compare_data(dataframe_cit, dataframe_tax)
     save_data(dataframe_missing)
     runtime = time.time() - start_time
