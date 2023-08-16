351beaf15ae3913fc3c07e1a6b418a8fe14dbafa
tj001 <43321167+timonj0@users.noreply.github.com>
2023-08-16 14:00:58 +0000

    Add programm structure

--- Finanzen/Steuern/Steuerregisterabgleich/v2/main.py
+++ Finanzen/Steuern/Steuerregisterabgleich/v2/main.py
@@ -2,3 +2,14 @@
 # Author: Timon Jakob
 # Date: 08.2023
 
+
+
+if __name__ == "__main__":
+    start_time = time.time()
+    dataframe_cit, dataframe_tax = load_data()
+    dataframe_cit = prepare_data_cit(dataframe_cit)
+    dataframe_tax = prepare_data_tax(dataframe_tax)
+    dataframe_missing = compare_data(dataframe_cit, dataframe_tax)
+    save_data(dataframe_missing)
+    runtime = time.time() - start_time
+    print_statistics(dataframe_cit, dataframe_tax, dataframe_missing, runtime)
