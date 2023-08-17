613db485257b249773deb2b5918269eaf0339784
tj001 <43321167+timonj0@users.noreply.github.com>
2023-08-17 07:04:45 +0000

    Update naming and config implementation

--- Finanzen/Steuern/Steuerregisterabgleich/v2/main.py
+++ Finanzen/Steuern/Steuerregisterabgleich/v2/main.py
@@ -1,19 +1,22 @@
 '''Steuerregisterabgleich v. 2.1'''
+# Version from 2.0: Based on AHV Codes
 # Author: Timon Jakob
 # Date: 08.2023
 
 import os
 import time
-
-import pandas
 import configparser
+
+import json
+import pandas
 
 CUTOFF_DATE = "01.01.1900"
 CUTOFF_DATE_FILTER = True
 CUTOFF_DATE_FIELD = "Zuzug_Datum"
 DATA_CIT_FORMAT = []
+CIT_AHV_FIELD = "Versichertennummer"
 
-AHV_FIELD = "AHV-VN"
+TAX_AHV_FIELD = "AHV-VN"
 
 def load_data():
     '''Function to load the data'''
@@ -71,11 +74,16 @@ def filter_cutoff_date(dataframe):
 
 def prepare_data_cit(dataframe):
     '''Function to prepare the data'''
-    if CUTOFF_DATE_FILTER
+    if CUTOFF_DATE_FILTER:
         # Filter the dataframe by the cutoff date
         dataframe = filter_cutoff_date(dataframe)
     # Remove all collumns from dataframe except for the ones listed in the config file
+    print(dataframe.head())
+    print(DATA_CIT_FORMAT)
     dataframe = dataframe[DATA_CIT_FORMAT]
+
+    # Rename AHV collumn
+    dataframe = dataframe.rename(columns={CIT_AHV_FIELD: TAX_AHV_FIELD})
     print("CIT Dataframe prepared successfully:")
     print(dataframe.head())
 
@@ -84,8 +92,8 @@ def prepare_data_cit(dataframe):
 def prepare_data_tax(dataframe_tax, dataframe_qst):
     '''Function to prepare the data'''
     # Preparing tax data got easy in v2. Only a dataframe with all the AHV-Codes of the entire tax register is needed.
-    dataframe_tax = dataframe_tax[[AHV_FIELD]]
-    dataframe_qst = dataframe_qst[[AHV_FIELD]]
+    dataframe_tax = dataframe_tax[[TAX_AHV_FIELD]]
+    dataframe_qst = dataframe_qst[[TAX_AHV_FIELD]]
 
     # Combine all dataframes
     dataframe_tax = pandas.concat([dataframe_tax, dataframe_qst], ignore_index=True)
@@ -102,7 +110,7 @@ def prepare_data_tax(dataframe_tax, dataframe_qst):
 def compare_data(dataframe_cit, dataframe_tax):
     '''Function to compare the data'''
     # Compare the dataframes
-    dataframe_missing = pandas.merge(dataframe_cit, dataframe_tax, on=AHV_FIELD, how='left', indicator=True)
+    dataframe_missing = pandas.merge(dataframe_cit, dataframe_tax, on=TAX_AHV_FIELD, how='left', indicator=True)
     dataframe_missing = dataframe_missing[dataframe_missing['_merge'] == 'left_only']
     dataframe_missing = dataframe_missing.drop(columns=['_merge'])
 
@@ -112,12 +120,12 @@ def compare_data(dataframe_cit, dataframe_tax):
     return dataframe_missing
 
 
-def save_data(dataframe_missing):
+def save_data(dataframe):
     '''Function to save the data'''
     # Save the dataframe
     # File name "register/fehlende_%D_%M_%Y__%H_%M_%S.xlsx"
     file_name = "register/fehlende_" + time.strftime("%d_%m_%Y__%H_%M_%S") + ".xlsx"
-    dataframe_missing.to_excel(file_name, index=False)
+    dataframe.to_excel(file_name, index=False)
     print("Dataframe saved successfully.")
 
 
@@ -135,11 +143,12 @@ if __name__ == "__main__":
     config = configparser.ConfigParser()
     config.read('config.cfg')
     CUTOFF_DATE = config['DATA']['CUTOFF_DATE']
-    CUTOFF_DATE_FILTER = config['DATA']['CUTOFF_DATE_FILTER']
+    CUTOFF_DATE_FILTER = config['DATA']['CUTOFF_DATE_FILTER'] == "True"
     CUTOFF_DATE_FIELD = config['DATA']['CUTOFF_DATE_FIELD']
-    DATA_CIT_FORMAT = config['DATA']['DATA_CIT_FORMAT']
+    DATA_CIT_FORMAT = json.loads(config['DATA']['DATA_CIT_FORMAT'])
+    CIT_AHV_FIELD = config['DATA']['CIT_AHV_FIELD']
 
-    AHV_FIELD = config['DATA']['AHV-FIELD']
+    TAX_AHV_FIELD = config['DATA']['TAX_AHV_FIELD']
 
     start_time = time.time()
     dataframe_cit, dataframe_tax, dataframe_qst = load_data()
