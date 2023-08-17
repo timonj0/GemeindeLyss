a31f01a4c1897ecb6f021e4982ad286ec0dbf53d
timon <timon.jakob@lyss.ch>
2023-08-17 13:02:33 +0200

    Update dependencies

--- Finanzen/Steuern/Steuerregisterabgleich/v2_GUI/setup.bat
+++ Finanzen/Steuern/Steuerregisterabgleich/v2_GUI/setup.bat
@@ -6,6 +6,8 @@ echo Installing Packages
 pip install pandas
 pip install openpyxl
 pip install configparser
+pip install tkinter
+pip install tkinterdnd2
 echo Packages installed successfully
 echo Setup complete!
 pause
