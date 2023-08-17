7068d97d81fc856ef01a36aeb267f66acf76a568
tj001 <43321167+timonj0@users.noreply.github.com>
2023-08-17 08:26:09 +0000

    Add setup.bat

---
+++ Finanzen/Steuern/Steuerregisterabgleich/v2/setup.bat
@@ -0,0 +1,11 @@
+@echo off
+echo Steuerregisterabgleich Setup
+echo Updating pip
+python -m pip install --upgrade pip
+echo Installing Packages
+pip install pandas
+pip install openpyxl
+pip install configparser
+echo Packages installed successfully
+echo Setup complete!
+pause
